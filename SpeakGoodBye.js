89dabd790fd23ee0a06f943569b44d5982359ff3
Drasko Nikolic <Drasko Nikolic>
2016-12-16 17:12:14 +0100

    change

--- SpeakGoodBye.js
+++ SpeakGoodBye.js
@@ -2,7 +2,7 @@
   var byeSpeaker ={};
   var speakWord = "Good Bye";
   byeSpeaker.speak = function (name) {
-  speakWord + " " + name;
+  return speakWord + " " + name;
   }
   window.byeSpeaker = byeSpeaker;
 })(window);
--- SpeakHello.js
+++ SpeakHello.js
@@ -2,7 +2,7 @@
   var helloSpeaker = {} ;
   var speakWord = "Hello";
   helloSpeaker.speak = function(name) {
-speakWord + " " + name;
+ return speakWord + " " + name;
   }
   window.helloSpeaker= helloSpeaker;
 })(window);
