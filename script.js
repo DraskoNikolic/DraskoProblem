5a4d5a3d62710c815e7079902d69911edf8e7d02
Drasko Nikolic <Drasko Nikolic>
2016-12-16 16:21:56 +0100

    problem

---
+++ script.js
@@ -0,0 +1,11 @@
+(function(window){
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+  for (var i=0; i< names.length; i++){
+    var firstLetter = names[i].charAt(0).toLowerCase();
+    if (firstLetter=== "j"){
+      console.log(byeSpeaker.speak(names[i]));
+    } else {
+      console.log(helloSpeaker.speak(names[i]));
+    }
+  }
+})(window);
